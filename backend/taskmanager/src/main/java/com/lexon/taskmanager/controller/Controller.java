6884cccd14a4ad83e4ac454e4dd2ff2bee411629
Lexon Schexnider <otherslexon@gmail.com>
2025-08-20 22:09:12 -0500

    Added DeleteMapping for deleting task (Not Tested)

--- backend/taskmanager/src/main/java/com/lexon/taskmanager/controller/Controller.java
+++ backend/taskmanager/src/main/java/com/lexon/taskmanager/controller/Controller.java
@@ -7,7 +7,9 @@ import java.util.ArrayList;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -45,4 +47,9 @@ public class Controller {
     
 
     //Deletes
+    @DeleteMapping("/tasks/{id}")
+    public String deleteTask(@PathVariable int newId) {
+        tasks.removeIf(task -> task.getId() == newId);
+        return "Task " + newId + " Deleted";
+    }
 }
